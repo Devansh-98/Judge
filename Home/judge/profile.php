00a2233abe6594fe32797f6f0e396ff3fbc7e6e7
Shubh2693 <shubhagrawal2693@gmail.com>
2019-01-19 01:27:39 +0530

    Welcome updated

--- Home/judge/profile.php
+++ Home/judge/profile.php
@@ -12,140 +12,11 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" type="text/css" href="try.css">
+    
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-        /* Navbar container */
-        .navbar {
-          overflow: hidden;
-          background-color: #333;
-          font-family: Arial;
-        }
-
-        /* Links inside the navbar */
-        .navbar a {
-          float: left;
-          font-size: 16px;
-          color: white;
-          text-align: center;
-          padding: 14px 16px;
-          text-decoration: none;
-        }
-
-        /* The dropdown container */
-        .dropdown {
-          float: left;
-          overflow: hidden;
-        }
-
-        /* Add a red background color to navbar links on hover */
-        .navbar a:hover, .dropdown:hover .dropbtn {
-          background-color: red;
-        }
-
-        .btn {
-            border: none;
-            color: white;
-            padding: 14px 28px;
-            font-size: 16px;
-            cursor: pointer;
-        }
-
-        body
-        {
-            background: -webkit-linear-gradient(left, #3931af, #00c6ff);
-        }
-        .emp-profile{
-            padding: 3%;
-            margin-top: 3%;
-            margin-bottom: 3%;
-            border-radius: 0.5rem;
-            background: #fff;
-        }
-        .profile-img{
-            text-align: center;
-        }
-        .profile-img img{
-            width: 70%;
-            height: 100%;
-        }
-        .profile-img .file {
-            position: relative;
-            overflow: hidden;
-            margin-top: -20%;
-            width: 70%;
-            border: none;
-            border-radius: 0;
-            font-size: 15px;
-            background: #212529b8;
-        }
-        .profile-img .file input {
-            position: absolute;
-            opacity: 0;
-            right: 0;
-            top: 0;
-        }
-        .profile-head h4{
-            color: #0062cc;
-        }
-        .profile-edit-btn{
-            border: none;
-            border-radius: 1.5rem;
-            width: 70%;
-            padding: 2%;
-            font-weight: 600;
-            color: #6c757d;
-            cursor: pointer;
-        }
-        .proile-rating{
-            font-size: 12px;
-            color: #818182;
-            margin-top: 5%;
-        }
-        .proile-rating span{
-            color: #495057;
-            font-size: 15px;
-            font-weight: 600;
-        }
-        .profile-head .nav-tabs{
-            margin-bottom:5%;
-        }
-        .profile-head .nav-tabs .nav-link{
-            font-weight:600;
-            border: none;
-        }
-        .profile-head .nav-tabs .nav-link.active{
-            border: none;
-            border-bottom:2px solid #0062cc;
-        }
-        .profile-work{
-            padding: 14%;
-            margin-top: -15%;
-        }
-        .profile-work p{
-            font-size: 12px;
-            color: #818182;
-            font-weight: 600;
-            margin-top: 10%;
-        }
-        .profile-work a{
-            text-decoration: none;
-            color: #495057;
-            font-weight: 600;
-            font-size: 14px;
-        }
-        .profile-work ul{
-            list-style: none;
-        }
-        .profile-tab label{
-            font-weight: 600;
-        }
-        .profile-tab p{
-            font-weight: 600;
-            color: #0062cc;
-        }
-    </style>
+   
 </head>
 <body>
 
@@ -162,7 +33,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
     
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+   
     
     <!------ Include the above in your HEAD tag ---------->
 
@@ -170,13 +43,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
                 <form method="post">
                     <div class="row">
                         <div class="col-md-4">
-                            <div class="profile-img">
+                            <!-- <div class="profile-img">
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt=""/>
                                 <div class="file btn btn-lg btn-primary">
                                     Change Photo
                                     <input type="file" name="file"/>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                         <div class="col-md-6">
                             <div class="profile-head">
